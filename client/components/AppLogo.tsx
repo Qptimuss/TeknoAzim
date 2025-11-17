ed48e5f4fa04c76c5eae5d4d874865ba16383f23
[dyad] <zeynepecemsezer5566@hotmail.com>
2025-11-17 20:48:26 +0300

    [dyad] Logo boyutunu büyütme - wrote 1 file(s)

--- client/components/AppLogo.tsx
+++ client/components/AppLogo.tsx
@@ -17,8 +17,8 @@ const LogoContent = (
     <img
       src={LOGO_IMAGE_URL}
       alt="TeknoAzim Logo"
-      // Görseli rozet konteynerine sığacak şekilde boyutlandırıyoruz. Increased size to h-10
-      className="h-10 w-auto object-contain" 
+      // Görseli rozet konteynerine sığacak şekilde boyutlandırıyoruz. Increased size to h-12
+      className="h-12 w-auto object-contain" 
     />
   </div>
 );
