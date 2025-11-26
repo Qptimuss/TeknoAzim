59b6454a64ed19c334d67ed78a5700e458b5868e
[dyad] <mehmetakif.msrli55@gmail.com>
2025-11-26 20:43:29 +0300

    [dyad] Vercel dağıtım hatasını düzelt - wrote 4 file(s) + extra files edited outside of Dyad

---
+++ api/index.ts
@@ -0,0 +1,5 @@
+import { createServer } from '../server';
+
+// Bu dosya, Vercel'in sunucusuz fonksiyonları için bir giriş noktası görevi görür.
+// Mevcut Express uygulamanızı içe aktarır ve dışa aktarır.
+export default createServer();
